d52eface5d3f15a73716a59a187a57fbb2fdcc69
Tolga Sirkeci <tsir8879@uni.sydney.edu.au>
2024-08-26 11:47:40 +1000

    Edited the convertor to go through a directory of txt files

--- label_convertor.py
+++ label_convertor.py
@@ -1,21 +1,31 @@
+import os
+
 def voc_data_calc(x_centre, y_centre, width, height):
     # Return x_min, y_min, x_max, y_max
     return (x_centre - width, y_centre - height, x_centre + width, y_centre + height)
 
-def yaml_to_voc(label_file):
-    with open(label_file, "r") as f:
-        for line in f.readlines():
-            yaml_data = line.split(" ")
+def yolotxt_to_voc(folder_path):
+    dir = os.listdir(folder_path)
+    data_arr = []
 
-            try:
-                object_label = int(yaml_data[0])
-                x_centre = float(yaml_data[1])
-                y_centre = float(yaml_data[2])
-                width = float(yaml_data[3])
-                height = float(yaml_data[4])
-            except(IndexError):
-                print("Missing values")
-                print(yaml_data)
+    for label_file in dir:
+        voc_data_arr = []
+        with open(label_file, "r") as f:
+            for line in f.readlines():
+                yolo_data = line.split(" ")
+                try:
+                    x_centre = float(yolo_data[1])
+                    y_centre = float(yolo_data[2])
+                    width = float(yolo_data[3])
+                    height = float(yolo_data[4])
+                except(IndexError):
+                    print("Missing values")
+                    print(yolo_data)
 
-            voc_data = voc_data_calc(x_centre, y_centre, width, height)
-            print(voc_data)
+                voc_data = voc_data_calc(x_centre, y_centre, width, height)
+                voc_data_arr.append(str(voc_data))
+
+        data_arr.append(voc_data_arr)
+
+    return data_arr
+            
--- label_program.py
+++ label_program.py
@@ -3,12 +3,13 @@ import sys
 
 if (len(sys.argv) != 3):
     print("Insufficient Number of Arguments")
+    sys.exit()
 
 if (sys.argv[1] == 'yaml_to_voc'):
-    try:
-        label_convertor.yaml_to_voc(sys.argv[2])
-    except(FileNotFoundError):
-        print("Labels does not exist")
+        try:
+            label_convertor.yaml_to_voc(sys.argv[3])
+        except(OSError):
+            print("Directory does not exist")
 
 else:
     print("Invalid Arguments")
