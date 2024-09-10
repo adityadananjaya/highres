62fddfcdec77f127e4b215a3fc9c5d878375fd18
Tolga Sirkeci <tsir8879@uni.sydney.edu.au>
2024-09-10 17:53:01 +1000

    Added documentation for label_convertor.py and label_program.py
    Co-authored-by: Broden Suffern <bsuffern@users.noreply.github.com>

--- label_convertor.py
+++ label_convertor.py
@@ -1,32 +1,58 @@
 import os
 
 def voc_data_calc(x_centre, y_centre, width, height):
-    # Return x_min, y_min, x_max, y_max
+    """
+    Compute VOC label data from YOLO label data: (x_centre, y_centre, width, heigth)
+    
+    Return tuple: integer label boundary in the form (x_min, y_min, x_max, y_max)
+    
+    Params:
+    x_centre: int
+    y_centre: int
+    width: int
+    height: int
+
+    """
+
     return (x_centre - width, y_centre - height, x_centre + width, y_centre + height)
 
 def yolotxt_to_voc(folder_path):
+    """
+    Converts YOLO data to VOC data format
+
+    Returns String[][] : array of string arrays
+
+    Params:
+    folder_path : String
+        directory containing all the label txt data
+
+    """
+
+    # Open directory containing labels
     dir = os.listdir(folder_path)
     data_arr = []
 
-    for label_file in dir:
+    for label_file in dir: # Loop over each label
 
         voc_data_arr = []
         with open(folder_path + '/' + label_file, "r") as f:
             for line in f.readlines():
-                # print(line)
+                # Read YOLO label data
                 yolo_data = line.split(" ")
-                try:
+                try: # Attempt to parse data as floats
                     x_centre = float(yolo_data[1])
                     y_centre = float(yolo_data[2])
                     width = float(yolo_data[3])
                     height = float(yolo_data[4])
-                except(IndexError):
+                except(IndexError): # Corrupted or incorrect format YOLO file
                     print("Missing values")
                     print(yolo_data)
 
+                # Compute and add VOC data tuple to list
                 voc_data = voc_data_calc(x_centre, y_centre, width, height)
                 voc_data_arr.append(str(voc_data))
 
+        # Add VOC string array to return list
         data_arr.append(voc_data_arr)
 
     return data_arr
--- label_program.py
+++ label_program.py
@@ -2,16 +2,18 @@ import label_convertor
 import sys
 import traceback
 
+# Check if user provided valid number of command-line arguments
 if (len(sys.argv) != 3):
     print("Insufficient Number of Arguments")
     sys.exit()
 
-if (sys.argv[1] == 'yolotxt_to_voc'):
-        try:
+if (sys.argv[1] == 'yolotxt_to_voc'): # From Yolo labels to VOC labels
+        try: # Error handling
             outputs = label_convertor.yolotxt_to_voc(sys.argv[2])
             print(outputs)
         except OSError as e:
             print(traceback.print_exc())
 
+# Print error message if invalid command-line arguments
 else:
     print("Invalid Arguments")
