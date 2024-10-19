712944d1a358dfd3a9dee31eb0a6f90421120b52
Tolga Sirkeci <tsir8879@uni.sydney.edu.au>
2024-10-19 16:44:42 +1100

    Added a trend plotter for compressions in huggingface_graph_generator.py

--- huggingface_graph_generator.py
+++ huggingface_graph_generator.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+
 def graph_model_data(dataset_name, stat, stat_idx, model_stats):
     """
     Produce the graphs
@@ -14,10 +15,12 @@ def graph_model_data(dataset_name, stat, stat_idx, model_stats):
     """
     models = []
     values = []
+
     # Obtain data
     for model in model_stats:
         models.append(model)
         values.append(model_stats[model][dataset_name][stat_idx])
+
     # Plot 
     plt.bar(models, values, color=['red', 'blue'], width=0.5)
     plt.title(stat + " over " + dataset_name + " dataset")
@@ -25,6 +28,47 @@ def graph_model_data(dataset_name, stat, stat_idx, model_stats):
     plt.ylabel(stat)
     # Save plots as images
     plt.savefig(stat + "_" + dataset_name + ".png")
+
+def graph_trend(stat, stat_idx, model_stats):
+    """
+    Produce a trend over each dataset
+    Params
+        stat : String
+            the statistic the graph is being generated over
+        stat_idx : Int
+            column containing the relevant statistics
+        model_stats : dictionary
+            the parsed data
+    """
+    models = []
+
+    # Obtain models
+    for model in model_stats:
+        models.append(model)
+
+    max_value = 0
+
+    plt.figure()
+
+    # Plot
+    for model in models:
+        values = []
+        datasets = []
+        for dataset in model_stats[model]:
+            datasets.append(dataset)
+            values.append(model_stats[model][dataset][stat_idx])
+            if model_stats[model][dataset][stat_idx] > max_value:
+                max_value = model_stats[model][dataset][stat_idx]
+        plt.plot(datasets, values, label = model)
+
+    plt.legend()
+    plt.xlim(min(datasets), max(datasets))
+    plt.ylim(0, max_value + max_value/10)
+    plt.title(stat + " over compressions" )
+    plt.xlabel("Compressions (%)")
+    plt.ylabel(stat)
+    plt.savefig(stat + "_" + "trend" + ".png")
+
 def generate_graph(csv):
     """
     Generates graphs using matplotlib of the results
@@ -54,12 +98,17 @@ def generate_graph(csv):
         else:
             if dataset not in model_stats[model_name]:
                 model_stats[model_name][dataset] = val_ls
-    for dataset in datasets_ref:
-        """
-        Average detections have an index of 0
-        Average confidence have an index of 1
-        """
-        graph_model_data(dataset, "Average Detections", 0, model_stats)
-        graph_model_data(dataset, "Average Confidence", 1, model_stats)
+
+    # for dataset in datasets_ref:
+    #     """
+    #     Average detections have an index of 0
+    #     Average confidence have an index of 1
+    #     """
+    #     graph_model_data(dataset, "Average Detections", 0, model_stats)
+    #     graph_model_data(dataset, "Average Confidence", 1, model_stats)
+
+    graph_trend("Average Detections", 0, model_stats)
+    graph_trend("Average Confidence", 1, model_stats)
+
 if __name__ == "__main__":
     generate_graph('/home/tsir/MachineLearning/capstone_project/models_performance_unlabelled.csv')
